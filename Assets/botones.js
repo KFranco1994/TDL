5b73106ea3b78c175e308a94bd1f14df84039228
KFranco1994 <kar.domiguezfra@google.com>
2020-04-27 21:48:40 -0500

    Añadimos los comentarios del código sobre el uso de los botones

--- Assets/botones.js
+++ Assets/botones.js
@@ -1,13 +1,14 @@
-﻿function BotonSalir()
+﻿//Para salir de la app
+function BotonSalir()
 {
   Application.Quit();
 }
-
+//Entrar a los submenus
 function BotonComenzarJuego()
 {
     Application.LoadLevel("submenudidactico");
 }
-
+//Visualizar los créditos
 function VerCreditos ()
 {
     Application.LoadLevel ("creditos");
